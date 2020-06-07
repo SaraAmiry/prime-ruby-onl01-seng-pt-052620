b10742415516f5fa326dacaeb3292a74880ab199
Sara Amiry <saraamiry24@gmail.com>
2020-06-07 07:57:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(num)
+    flag = true
+    for x in 2..num-1
+        if num % x == 0
+            flag = false
+        end
+    end
+    return flag
+end
+def is_prime(num)
+  n = 2
+  while n < num
+    return false
+  if num % n == 0
+   return true # n += 1
+  end
+end
